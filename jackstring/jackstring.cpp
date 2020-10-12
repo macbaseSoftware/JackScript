089590f6c80663b6f4621badd59d8227bc5d3a92
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-10-13 00:01:38 +0200

    added a 'includes' strng function

--- jackstring/jackstring.cpp
+++ jackstring/jackstring.cpp
@@ -4,12 +4,30 @@
 #include "jackstring.h"
 
 namespace js{
-int jackLength(std::string str){
-	int count = 0;
-	for (int i = 0; i < str.length(); ++i)
-	{
-		count++;
+	int length(std::string str){
+		int count = 0;
+		for (int i = 0; i < str.length(); ++i){
+			count++;
+		}
+		return count;
 	}
-	return count;
-}
+
+	int includes(std::string text, std::string str){
+		int macro = length(str);
+		int micro = length(text);
+		int j;
+
+		for (int i = 0; i < macro - micro; ++i){
+			for (j =0 ; j < micro; ++j){
+				if(str[i+j] != text[j]){
+					break;	
+				}		
+			}	
+
+			if(j == micro){
+				return i;
+			}
+		}
+		return -1;
+	}
 }
--- jackstring/jackstring.h
+++ jackstring/jackstring.h
@@ -1,19 +1,15 @@
 #ifndef JACKSTRING_H
 #define JACKSTRING_H
+#pragma once
 
 #include <iostream>
 #include <vector>
 #include <string>
 
-// class Jackstring{
-// 	private: 
-// 	std::string str;
 
-// 	public:
-// 	Jackstring();	
-// }
 namespace js{
-	int jackLength(std::string str);
+	int length(std::string str);
+	int includes(std::string text, std::string str); // returns 1 else -1 
 	
 }
 
